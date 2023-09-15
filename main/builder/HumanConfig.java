6d2a7dd7f730917732d237800433e272e4bad8f0
张皓然 <zhanghaoran@zens-MacBook-Pro.local>
2023-09-16 00:17:50 +0800

    h

--- main/builder/HumanConfig.java
+++ main/builder/HumanConfig.java
@@ -1,2 +1,12 @@
-package com.example.design.main.builder;public class Builder {
+package com.example.design.main.builder;
+
+import java.util.List;
+
+public class HumanConfig {
+    private double height;
+    private boolean gender;
+    private int age;
+    private String address;
+    private String Education;
+    private List<String> Internship;
 }
